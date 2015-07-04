b71bbcc9abe8ae6aa53b200ed9f5f58a68fd3864
Imran Mohsin <simohsin@hotmail.com>
2015-07-05 00:33:42 +0430

    coupon updates

--- resources/views/coupons.blade.php
+++ resources/views/coupons.blade.php
@@ -4,7 +4,8 @@
 
 <h3>Coupons</h3>
 <table  cellpadding="10" border="1">
-<tr><td>Coupon Code</td>
+<tr><td>Campaign Id</td>
+    <td>Coupon Code</td>
 	<td>Value</td>
 	<td>Is Permanent</td>
 	<td>Use Once</td>
@@ -20,20 +21,20 @@
 	<td>{{--delete--}}</td></tr>
 @foreach ($coupons as $coupon)
    <tr><td>{{ $coupon->campaign_id }}</td>
-    		<td>{{ $coupon->coupon_code }}</td>
-    		<td>{{ $coupon->value }}</td>
-    		<td>{{ $coupon->is-percentage }}</td>
-    		<td>{{ $coupon->use-once }}</td>
-    		<td>{{ $coupon->is-used }}</td>
-    		<td>{{ $coupon->active }}</td>
-    		<td>{{ $coupon->every_product }}</td>
-    		<td>{{ $coupon->start }}</td>
-    		<td>{{ $coupon->expiry }}</td>
-    		<td>{{ $coupon->condition }}</td>
-    		<td>{{ $coupon->coupon_redeemed }}</td>
-    		<td>{{ $coupon->coupon_date_redeemed }}</td>
-    		<td><a href="{{ action('CouponController@edit', $coupon->campaign_id) }}">Edit</a></td>
-    		<td><a href="{{ action('CouponController@delete', $coupon->campaign_id) }}">Delete</a></td>
+		<td>{{ $coupon->coupon_code }}</td>
+		<td>{{ $coupon->value }}</td>
+		<td>{{ $coupon->is_percentage }}</td>
+		<td>{{ $coupon->use_once }}</td>
+		<td>{{ $coupon->is_used }}</td>
+		<td>{{ $coupon->active }}</td>
+		<td>{{ $coupon->every_product }}</td>
+		<td>{{ $coupon->start }}</td>
+		<td>{{ $coupon->expiry }}</td>
+		<td>{{ $coupon->condition }}</td>
+		<td>{{ $coupon->coupon_redeemed }}</td>
+		<td>{{ $coupon->coupon_date_redeemed }}</td>
+		<td><a href="{{ action('CouponController@edit', ['id' => $coupon->campaign_id,'code' => $coupon->coupon_code]) }}">Edit</a></td>
+		<td><a href="{{ action('CouponController@delete', $coupon->campaign_id) }}">Delete</a></td>
    </tr>
 @endforeach
 <tr><td colspan="100%" align="right"><a href="{{ action('CouponController@create') }}">Add</a></td></tr>
